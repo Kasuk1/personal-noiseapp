aafd251a85bb70555e0c324e2ab011e040cda9c5
Igor Chinchay <igor2008_11@hotmail.com>
2021-12-28 16:59:30 -0500

    Conditional User Info Image

--- src/components/user-info/UserInfoDetail.jsx
+++ src/components/user-info/UserInfoDetail.jsx
@@ -13,7 +13,9 @@ export const UserInfoDetail = () => {
     //* Rendering final current-user-info
     return (
         <div className="user-info">
-            <div className="user-info__image" style={{backgroundImage: `url(${images[0].url})` }}></div>
+            <div className="user-info__image"
+                style={{ backgroundImage: `url(${images[0].url ? images[0].url : 'https://cdn-icons-png.flaticon.com/512/149/149071.png'})` }}>
+            </div>
             <h1 className="user-info__name">{display_name}</h1>
             <p className="user-info__followers">{followers.total} followers</p>
             <p className={product === "premium" ? "user-info-product user-info-product__premium" : "user-info-product user-info-product__normal"}>{product}
