440b51188e46ced40e97bb5244f33b5c085a9a12
Igor Chinchay <igor2008_11@hotmail.com>
2022-03-09 16:33:10 -0500

    Modified container and user-info styles

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,15 +1,19 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Alert } from '../components/Alert';
+import { Navbar } from '../components/layout/Navbar/Navbar';
 
 import { PublicRoutes } from './PublicRoutes';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Switch>
         <Route path='/'>
           <PublicRoutes />
         </Route>
       </Switch>
+      <Alert />
     </BrowserRouter>
   );
 };
